97fd74fcd2cfa4d91fd9105152d7781079897632
sumithMedamina <sumith.medamina@solventek.com>
2024-08-22 15:36:50 +0530

    added products and orders collections

--- index.js
+++ index.js
@@ -119,14 +119,13 @@ async function getFieldNamesFromCollection(collectionName) {
 // GET endpoint to retrieve field names
 app.get('/fields-data', async (req, res) => {
   try {
-      const organizationFields = await getFieldNamesFromCollection('organization');
-      const scriptFields = await getFieldNamesFromCollection('script');
-      const userFields = await getFieldNamesFromCollection('ottuser');
+      const productFields = await getFieldNamesFromCollection('products');
+      const ordersFields = await getFieldNamesFromCollection('orders');
+      
 
       const dict_data = {
-          'Organisation': organizationFields,
-          'Script': scriptFields,
-          'User': userFields
+          'products': productFields,
+          'orders': ordersFields,
       };
 
       res.json(dict_data);
@@ -138,147 +137,68 @@ app.get('/fields-data', async (req, res) => {
 // POST endpoint for generating reports
 app.post('/generate-reports', async (req, res) => {
   try {
-      const { organisation_fields, script_fields, user_fields, conditions } = req.body;
+    const { products_fields, order_fields, conditions } = req.body;
 
-      // Ensure validConditions is defined, even if it's an empty object
-      const validConditions = conditions || {};
+    // Ensure validConditions is defined, even if it's an empty object
+    const validConditions = conditions || {};
 
-      // Separate the conditions for each collection
-      const userConditions = {};
-      const organizationConditions = {};
-      const scriptConditions = {};
+    // Separate the conditions for each collection
+    const productConditions = {};
+    const orderConditions = {};
 
-      // Split conditions based on which collection the fields belong to
-      for (const [key, value] of Object.entries(validConditions)) {
-          if (user_fields.includes(key)) {
-              userConditions[key] = value;
-          }
-          if (organisation_fields.includes(key)) {
-              organizationConditions[key] = value;
-          }
-          if (script_fields.includes(key)) {
-              scriptConditions[key] = value;
-          }
+    // Helper function to determine and convert the data type dynamically
+    const convertConditionValue = async (collectionName, key, value) => {
+      const sampleDoc = await mongoose.connection.collection(collectionName).findOne({}, { projection: { [key]: 1 } });
+      const fieldValue = sampleDoc ? sampleDoc[key] : null;
+
+      if (fieldValue !== null && typeof fieldValue !== typeof value) {
+        if (typeof fieldValue === 'number') {
+          return parseFloat(value);
+        } else if (fieldValue instanceof Date) {
+          return new Date(value);
+        }
+        // Add more type checks if needed
       }
+      return value;
+    };
 
-      // Build projections for each collection
-      const userProjection = user_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
-      const organizationProjection = organisation_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
-      const scriptProjection = script_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
+    // Split conditions based on which collection the fields belong to
+    for (const [key, value] of Object.entries(validConditions)) {
+      if (products_fields.includes(key)) {
+        productConditions[key] = await convertConditionValue('products', key, value);
+      }
+      if (order_fields.includes(key)) {
+        orderConditions[key] = await convertConditionValue('orders', key, value);
+      }
+    }
 
-      // Query each collection with the relevant conditions and projections
-      const users = await mongoose.connection.collection('ottuser')
-          .find(userConditions)
-          .project(userProjection)
-          .toArray();
+    // Build projections for each collection
+    const productProjection = products_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
+    const orderProjection = order_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
 
-      const organization_data = await mongoose.connection.collection('organization')
-          .find(organizationConditions)
-          .project(organizationProjection)
-          .toArray();
+    // Query each collection with the relevant conditions and projections
+    const products = await mongoose.connection.collection('products')
+      .find(productConditions)
+      .project(productProjection)
+      .toArray();
 
-      const scripts_data = await mongoose.connection.collection('script')
-          .find(scriptConditions)
-          .project(scriptProjection)
-          .toArray();
+    const orders = await mongoose.connection.collection('orders')
+      .find(orderConditions)
+      .project(orderProjection)
+      .toArray();
 
-      // Combine the data into a single response
-      const response_data = {
-          "organisation": organization_data,
-          "script": scripts_data,
-          "user": users
-      };
+    // Combine the data into a single response
+    const response_data = {
+      "products": products,
+      "orders": orders,
+    };
 
-      res.json(response_data);
+    res.json(response_data);
   } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).json({ error: error.message });
+    console.error('Error:', error.message);
+    res.status(500).json({ error: error.message });
   }
 });
 
-// Helper function to get field names dynamically from MongoDB collections
-async function getFieldNamesFromCollection(collectionName) {
-    const collection = mongoose.connection.collection(collectionName);
-    const document = await collection.findOne({});
-    return document ? Object.keys(document) : [];
-}
 
-// GET endpoint to retrieve field names
-app.get('/fields-data', async (req, res) => {
-    try {
-        const organizationFields = await getFieldNamesFromCollection('organization');
-        const scriptFields = await getFieldNamesFromCollection('script');
-        const userFields = await getFieldNamesFromCollection('ottuser');
 
-        const dict_data = {
-            'Organisation': organizationFields,
-            'Script': scriptFields,
-            'User': userFields
-        };
-
-        res.json(dict_data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-// POST endpoint for generating reports
-app.post('/generate-reports', async (req, res) => {
-    try {
-        const { organisation_fields, script_fields, user_fields, conditions } = req.body;
-
-        // Ensure validConditions is defined, even if it's an empty object
-        const validConditions = conditions || {};
-
-        // Separate the conditions for each collection
-        const userConditions = {};
-        const organizationConditions = {};
-        const scriptConditions = {};
-
-        // Split conditions based on which collection the fields belong to
-        for (const [key, value] of Object.entries(validConditions)) {
-            if (user_fields.includes(key)) {
-                userConditions[key] = value;
-            }
-            if (organisation_fields.includes(key)) {
-                organizationConditions[key] = value;
-            }
-            if (script_fields.includes(key)) {
-                scriptConditions[key] = value;
-            }
-        }
-
-        // Build projections for each collection
-        const userProjection = user_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
-        const organizationProjection = organisation_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
-        const scriptProjection = script_fields.reduce((acc, field) => ({ ...acc, [field]: 1 }), {});
-
-        // Query each collection with the relevant conditions and projections
-        const users = await mongoose.connection.collection('ottuser')
-            .find(userConditions)
-            .project(userProjection)
-            .toArray();
-
-        const organization_data = await mongoose.connection.collection('organization')
-            .find(organizationConditions)
-            .project(organizationProjection)
-            .toArray();
-
-        const scripts_data = await mongoose.connection.collection('script')
-            .find(scriptConditions)
-            .project(scriptProjection)
-            .toArray();
-
-        // Combine the data into a single response
-        const response_data = {
-            "organisation": organization_data,
-            "script": scripts_data,
-            "user": users
-        };
-
-        res.json(response_data);
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).json({ error: error.message });
-    }
-});
